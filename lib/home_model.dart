3e7287e146385177d4396774a8318b79bd992bf8
yuto90 <solidusgako4087@gmail.com>
2022-03-02 15:07:16 +0900

    拠点生成の時に被らない一意の座標を返却する

--- lib/home_model.dart
+++ lib/home_model.dart
@@ -7,24 +7,49 @@ class HomeModel extends ChangeNotifier {
     generateBaseDetails();
   }
 
-  // ランダムなdoubleを生成する(1 ~ -1)
-  double randomDouble() {
-    if (Random().nextBool()) {
-      return Random().nextDouble();
-    } else {
-      return (Random().nextDouble() - 1);
+  // 生成拠点の座標組み合わせ格納用
+  List positionSetAll = [];
+
+  // 拠点の座標を生成して返却する
+  // todo もっと効率いい記述あるかも
+  List<double> generateXY() {
+    List<double> positionInt = [-0.2, -0.5, -0.7, 0.2, 0.5, 0.7];
+    List<double> positionSet = [];
+    bool reloop = true;
+
+    while (reloop) {
+      // 初期化
+      positionSet = [];
+      reloop = false;
+
+      // XYの組み合わせリストを作る
+      for (int i = 0; i <= 1; i++) {
+        positionSet.add(positionInt[Random().nextInt(positionInt.length - 1)]);
+      }
+
+      // 作成した組み合わせが重複していないかを調べる
+      for (List position in positionSetAll) {
+        // 組み合わせがかぶっていれば再度作り直し
+        if (position[0] == positionSet[0] && position[1] == positionSet[1]) {
+          reloop = true;
+          break;
+        }
+      }
     }
+
+    positionSetAll.add(positionSet);
+    return positionSet;
   }
 
   // 各拠点の詳細データを生成
   void generateBaseDetails() {
     for (int i = 2; i <= 9; i++) {
-      double x = randomDouble();
-      double y = randomDouble();
+      // 一意の座標を生成
+      List<double> xy = generateXY();
 
       allBaseDetails[i.toString()] = {
-        'x': x,
-        'y': y,
+        'x': xy[0],
+        'y': xy[1],
         'control': 2,
         'scale': 0,
       };
@@ -59,14 +84,14 @@ class HomeModel extends ChangeNotifier {
 // 全ての拠点の情報
   Map<String, Map> allBaseDetails = {
     '0': {
-      'x': 1.0, // 拠点のX座標
-      'y': 1.0, // 拠点のY座標
+      'x': 0.8, // 拠点のX座標
+      'y': 0.9, // 拠点のY座標
       'control': 0, // 拠点の支配下(0:味方, 1:敵)
       'scale': 100, // 拠点の戦力パラメータ
     }, //自分の本陣
     '1': {
-      'x': -1.0,
-      'y': -1.0,
+      'x': -0.8,
+      'y': -0.9,
       'control': 1,
       'scale': 100,
     }, //敵の本陣
