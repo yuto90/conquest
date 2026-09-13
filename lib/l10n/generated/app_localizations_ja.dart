43fe1767dc1943c4550009f9a12de9058a9690ab
yuto90 <53107639+yuto90@users.noreply.github.com>
2026-09-14 04:10:30 +0900

    fix(web): align app title with CONQUEST ISLES (#69)

--- lib/l10n/generated/app_localizations_ja.dart
+++ lib/l10n/generated/app_localizations_ja.dart
@@ -12,6 +12,9 @@ class AppLocalizationsJa extends AppLocalizations {
   String get brandName => 'CONQUEST';
 
   @override
+  String get appTitle => 'CONQUEST ISLES';
+
+  @override
   String get titleStart => 'はじめる';
 
   @override
